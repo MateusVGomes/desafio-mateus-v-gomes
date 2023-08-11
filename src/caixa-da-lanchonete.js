f5d103aee7c5591f1dec672911f9a91cce0c6cc4
Mateus V. Gomes <monkmitogames@hotmail.com>
2023-08-11 17:01:45 +0000

    Validação de quantidade inserida

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,7 +1,89 @@
+const cardapio = [
+    {
+      codigo: "cafe",
+      descricao: "Café",
+      valor: 3.00,
+      extra: false
+    },
+    {
+      codigo: "chantily",
+      descricao: "Chantily (extra do Café)",
+      valor: 1.50,
+      extra: true
+    },
+    {
+      codigo: "suco",
+      descricao: "Suco Natural",
+      valor: 6.20,
+      extra: false
+    },
+    {
+      codigo: "sanduiche",
+      descricao: "Sanduíche",
+      valor: 6.50,
+      extra: false
+    },
+    {
+      codigo: "queijo",
+      descricao: "Queijo (extra do Sanduíche)",
+      valor: 2.00,
+      extra: true
+    },
+    {
+      codigo: "salgado",
+      descricao: "Salgado",
+      valor: 7.25,
+      extra: false
+    },
+    {
+      codigo: "combo1",
+      descricao: "1 Suco e 1 Sanduíche",
+      valor: 9.50,
+      extra: false
+    },
+    {
+      codigo: "combo2",
+      descricao: "1 Café e 1 Sanduíche",
+      valor: 7.50,
+      extra: false
+    },
+  
+  ] 
+
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+        const objetoDeItens = [];
+  let mensagem = "";
+  itens.forEach((str) => {
+    let arr = str.split(",");
+    let atual = parseFloat(arr[1]);
+       if (atual<=0||mensagem!=""){
+             mensagem = "Quantidade inválida!";
+           return mensagem;
+    }
+    
+    else{
+
+    objetoDeItens.push({
+      codigo: arr[0],
+      quantidade: arr[1]
+    })
+      
+      }
+  })
+if(mensagem!==""){
+  return mensagem;
+}
+else{
+  const pedidosDetalhados = objetoDeItens.map(pedido => {
+    const itemCardapio = cardapio.find(item => item.codigo === pedido.codigo)
+    return { ...itemCardapio, quantidade: pedido.quantidade }
+  });
+
+  return pedidosDetalhados;
+}
+
     }
 
 }
